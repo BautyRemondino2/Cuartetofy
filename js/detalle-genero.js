0b989f156adfa5e819b0e3eb2cb62efab3f022cb
lolo-anino <lanino@udesa.edu.ar>
2022-06-20 03:42:41 -0300

    todavia la andan buscando, bebiendo se fue y no a vuelto

--- js/detalle-genero.js
+++ js/detalle-genero.js
@@ -3,6 +3,7 @@ let qString = new URLSearchParams(String);
 let idDet = qString.get('id')
 
 let urldetail = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/${idDet}`
+let urlDetailArt = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/`
 
 fetch(urldetail)
 .then(function(ans){
@@ -21,6 +22,26 @@ fetch(urldetail)
                   </atricle>`
                   
   
+
+})
+fetch(urlDetailArt + idDet + "/artists")
+    .then(function(ans) {
+        return ans.json();            
+})
+    .then(function(dArtist) {
+        console.log (dArtist)
+
+        let detalleArtistas = document.querySelector(".detailArtist")
+
+
+        for(let i=0; i<=dArtist.data.length; i++) {           
+            detalleArtistas.innerHTML +=`
+                    <article class="detailArtist">
+                    <img class="cordoba" src="${dArtist.data[i].picture}" alt="">
+                    <p class="">${dArtist.data[i].name}</p>
+                    </article>`
+            }
+
 
 })
 .catch(function(error){
